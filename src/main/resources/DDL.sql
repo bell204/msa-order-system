66798f24cb1f1b9be36efa59a4fc710d33aefc7c
beginclass2017 <beginclass2017@gmail.com>
2026-03-29 21:43:56 +0900

    주문 테이블 생성

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -1,8 +1,7 @@
-CREATE TABLE TB_ORDER (
-    id SERIAL PRIMARY KEY,
-    order_id VARCHAR(50) NOT NULL,
-    user_id VARCHAR(50) NOT NULL,
-    item_name VARCHAR(100) NOT NULL,
-    amount INT NOT NULL,
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE tb_order (
+    order_id    VARCHAR(50) PRIMARY KEY,
+    user_id     VARCHAR(50) NOT NULL,
+    item_name   VARCHAR(100) NOT NULL,
+    amount      NUMERIC(12, 2) NOT NULL,
+    order_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
