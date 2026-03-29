c8d0fc280f50ea80698b70c3c38c379f33ea3b63
beginclass2017 <beginclass2017@gmail.com>
2026-03-29 23:01:50 +0900

    객체 생성 위해 생성자 생성 어노테이션 추가

--- common-module/src/main/java/com/bienbetter/common/event/OrderCreatedEvent.java
+++ common-module/src/main/java/com/bienbetter/common/event/OrderCreatedEvent.java
@@ -1,11 +1,16 @@
 package com.bienbetter.common.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;              // Add this for @Data
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;   // Add this for LocalDateTime
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreatedEvent {
     private String orderId;
     private String userId;
